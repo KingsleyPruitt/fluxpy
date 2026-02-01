fe9b431e43bbede98984884ab3aa2ed958141594
kingpin <king.pin@tutamail.com>
2026-02-01 11:57:54 -0500

    .

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mypackage",  
-    version="0.1.0",  
-    packages=find_packages(),
+    name="fluxpy",  # Package name is fluxpy
+    version="0.1.0",  # Version of the package
+    packages=find_packages(),  # This finds all the submodules
     install_requires=[],  # List any dependencies here
     author="Your Name",
     author_email="your_email@example.com",
-    description="A simple package to greet users",
+    description="A simple package for fluxpy functionality",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/mypackage",
+    url="https://github.com/yourusername/fluxpy",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -20,3 +20,4 @@ setup(
     ],
     python_requires='>=3.6',
 )
+
